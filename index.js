b13cd653208e057666b0056e077ca5ea0fbd3257
Pon Huang <carolyn722@gmail.com>
2022-07-04 17:31:12 +0800

    javascript

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const btnToggle = document.querySelector(".btn");
+const form = document.querySelector("#form");
+const email = document.querySelector(".email");
+const btnSubmit = document.querySelector(".btn-submit");
 
-function validateEmail(input) {
-  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (input.value.match(mailFormat)) {
-    alert("yeah! a valid email");
-    document.emailAdress.focus();
-    return true;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateEmail();
+});
+
+// function validateInput() {
+//   // get the valus from the input
+//   // trim() to remove the extra space
+//   const emailValue = email.value.trim();
+
+//   if (emailValue === "") {
+//     //show error
+//     // remove hidden class
+//     setErrorFor(email, "Email cannot be blank");
+//   } else {
+//     // keep hidden class
+//     setSuccessFor(email);
+//   }
+// }
+
+function validateEmail(email) {
+  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.value.match(emailFormat)) {
+    email.classList.add("success");
   } else {
     document.querySelector(".notification").classList.remove(hidden);
-
-    document.querySelector("form .icon").classList.remove(hidden);
+    document.querySelector("icon").classList.remove(hidden);
   }
 }
