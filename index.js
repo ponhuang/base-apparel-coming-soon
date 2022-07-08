f2bb29b248d7c3575b421bcb4c62df4ab9d01e8b
Pon Huang <carolyn722@gmail.com>
2022-07-08 18:17:47 +0800

    error icon overflow fix

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // regex Email
-// /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// ^ this symbol represents beginning of input
+//  \w  this means any upperCase character, lowerCase character, number or underscore
+//  +   means: one or more characters
+// ([\.-]?\w)  means, a dot '.' or hyphen '- '.  ('?' means: but there must be a character after it)
+//  *   means: zero or more combinations of ([\.-]?\w)
+// @\w+  means: @ and one or more characters after it
+// (\.\w{2,3})+$  this means at the end, there must be a dot, then, 2-3 characters like .com, .eg, uk or .net
+// $ this represents end of the input
 
 const form = document.querySelector("form");
 const email = document.querySelector(".email");
 const errorMessage = document.querySelector(".notification");
 const errorIcon = document.querySelector(".icon");
-const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 form.addEventListener("submit", (e) => {
   // prevent form from submitting
